ea56bf64f9c63c642403624a0cf5ccb0a6d8c81b
Vinicius Fillos <vinifillos@gmail.com>
2025-03-17 10:39:31 -0300

    initial commit

---
+++ src/main/java/io/github/vinifillos/Main.java
@@ -0,0 +1,39 @@
+package io.github.vinifillos;
+
+import org.apache.fop.apps.*;
+
+import javax.xml.transform.*;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        // input
+        File xmlFile = new File("src/main/resources/data/input.xml");
+        File xsltFile = new File("src/main/resources/style/style.xsl");
+
+        // output
+        File pdfFile = new File("src/main/resources/pdf/output.pdf");
+        pdfFile.getParentFile().mkdirs();
+
+        FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+        FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+
+        try (OutputStream out = new FileOutputStream(pdfFile)) {
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(xsltFile));
+
+            Source src = new StreamSource(xmlFile);
+            Result res = new SAXResult(fop.getDefaultHandler());
+
+            transformer.transform(src, res);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
